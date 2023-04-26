03819b2f85eba81bbafe66147bd899887ee70dd6
manwithoutsleep <brad@southcountysound.com>
2023-04-25 20:31:39 -0500

    feat: Add dependency injection

--- VehiclePhotography.App.DAO/Interfaces/IVehicleInfoDao.cs
+++ VehiclePhotography.App.DAO/Interfaces/IVehicleInfoDao.cs
@@ -3,7 +3,7 @@ using VehiclePhotography.App.Domain.Values;
 
 namespace VehiclePhotography.App.DAO.Interfaces
 {
-    internal interface IVehicleInfoDao
+    public interface IVehicleInfoDao
     {
         public Task<VehicleInfo> GetVehicleInfoAsync(string vin);
     }
